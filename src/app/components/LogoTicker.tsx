4342894a8dc70cb9a877e2b0925f48be0738e98b
6syro9 <abdul.mohammed@op.iitg.ac.in>
2024-11-12 21:57:32 +0530

    Update LogoTicker.tsx

--- src/app/components/LogoTicker.tsx
+++ src/app/components/LogoTicker.tsx
@@ -1,36 +1,43 @@
 'use client';
-import React from 'react'
-import Image from 'next/image'
-import { motion } from 'framer-motion'
+import React from 'react';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+
 const LogoTicker = () => {
   return (
-    <div className='py-6 bg-white md:py-12'>
-        <div className='flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]'>
-                <div className='container'>
-                  <motion.div className='flex gap-14 flex-none pr-14' animate={{translateX:"-50%"}}
-                  transition={{
-                    duration:18,
-                    repeat:Infinity,
-                    ease:"linear",
-                    repeatType:"loop"
+    <div className="py-6 bg-white md:py-12">
+      <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+        <div className="container">
 
-                  }}>
-                    <Image src={'/logo-acme.png'} width={200} height={200} alt=''></Image>
-                    <Image src={'/logo-quantum.png'} width={200} height={200} alt=''></Image>
-                    <Image src={'/logo-celestial.png'} width={200} height={200} alt=''></Image>
-                    <Image src={'/logo-pulse.png'} width={200} height={200} alt=''></Image>
-                    <Image src={'/logo-apex.png'} width={200} height={200} alt=''></Image>
-                  {/* second set of logos for the animation */}
-                    <Image src={'/logo-acme.png'} width={200} height={200} alt=''></Image>
-                    <Image src={'/logo-quantum.png'} width={200} height={200} alt=''></Image>
-                    <Image src={'/logo-celestial.png'} width={200} height={200} alt=''></Image>
-                    <Image src={'/logo-pulse.png'} width={200} height={200} alt=''></Image>
-                    <Image src={'/logo-apex.png'} width={200} height={200} alt=''></Image>
-                    </motion.div>
-                </div> 
-        </div>        
+          <motion.div
+            className="flex gap-14 flex-none pr-14"
+            animate={{
+              x: "-100%", 
+            }}
+            transition={{
+              duration: 18, 
+              repeat: Infinity, 
+              ease: "linear", 
+            }}
+          >
+            {/* Logos for the ticker */}
+            <Image src="/logo-acme.png" width={200} height={200} alt="Acme" />
+            <Image src="/logo-quantum.png" width={200} height={200} alt="Quantum" />
+            <Image src="/logo-celestial.png" width={200} height={200} alt="Celestial" />
+            <Image src="/logo-pulse.png" width={200} height={200} alt="Pulse" />
+            <Image src="/logo-apex.png" width={200} height={200} alt="Apex" />
+
+            {/* Second set of logos to loop */}
+            <Image src="/logo-acme.png" width={200} height={200} alt="Acme" />
+            <Image src="/logo-quantum.png" width={200} height={200} alt="Quantum" />
+            <Image src="/logo-celestial.png" width={200} height={200} alt="Celestial" />
+            <Image src="/logo-pulse.png" width={200} height={200} alt="Pulse" />
+            <Image src="/logo-apex.png" width={200} height={200} alt="Apex" />
+          </motion.div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LogoTicker
+export default LogoTicker;
